baec33cda5f747bd1846025ff2af0a42af1940a4
HeZaiJin <1376277145@qq.com>
2016-02-02 18:38:57 +0800

    change

--- widgets/src/main/java/com/hezaijin/advance/widgets/view/ptr/load/PtrAdapter.java
+++ widgets/src/main/java/com/hezaijin/advance/widgets/view/ptr/load/PtrAdapter.java
@@ -1,24 +1,100 @@
 package com.hezaijin.advance.widgets.view.ptr.load;
 
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * 如果列表的高度小于屏幕的高度
+ * 可以判断滑动到最底部的时候，设置footview为加载中
  *
  * @author HaoZhang
  * @date 2016/2/1.
  */
 public class PtrAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public static final int MODE_BOTH = 0;
+    public static final int MODE_ONLY_PULLDOWN = -1;
+    private RecyclerView.Adapter<RecyclerView.ViewHolder> mAdapter;
+    private int mMode = MODE_BOTH;
+    private OnItemClickListener mOnItemClickListener;
+    private OnItemLongClickListener mOnItemLongClickListener;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    public PtrAdapter(int mode, RecyclerView.Adapter adapter) {
+
+        setAdapter(adapter);
+    }
+
+    public void setAdapter(RecyclerView.Adapter<RecyclerView.ViewHolder> adapter) {
+        if (null != adapter) {
+            if (!(adapter instanceof RecyclerView.Adapter)) {
+                throw new RuntimeException("A RecyclerView.Adapter is Need");
+            }
+
+            if (null != mAdapter) {
+                notifyItemRangeRemoved(getHeadersCount(), mAdapter.getItemCount());
+                mAdapter.unregisterAdapterDataObserver(mDataObserver);
+            }
+            mAdapter = adapter;
+            mAdapter.registerAdapterDataObserver(mDataObserver);
+            notifyItemRangeInserted(getHeadersCount(), mAdapter.getItemCount());
+        }
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (null != mAdapter) {
+            if (mHeaderViewTypes.contains(viewType)) {
+                //currentPosition in mHeaderViews is (viewType - TYPE_HEADER_VIEW)
+                return new RecyclerHeaderViewHolder(mHeaderViews.get(viewType - TYPE_HEADER_VIEW));
+            } else if (mFooterViewTypes.contains(viewType)) {
+                //currentPosition in mFooterViews is (viewType - headersCount - TYPE_FOOTER_VIEW)
+                return new RecyclerHeaderViewHolder(mFooterViews.get(viewType - getHeadersCount()
+                        - TYPE_FOOTER_VIEW));
+            } else {
+                return mAdapter.onCreateViewHolder(parent, viewType);
+            }
+        }
+        return null;
+    }
 
+    public RecyclerView.Adapter getAdapter() {
+        return mAdapter;
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        if (null != mAdapter) {
+            if (position >= getHeadersCount() && position < getHeadersCount() + mAdapter.getItemCount()) {
+                mAdapter.onBindViewHolder(holder, position - getHeadersCount());
+                if (null != mOnItemClickListener) {
+                    holder.itemView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mOnItemClickListener.onItemClick(holder, position - getHeadersCount());
+                        }
+                    });
+                }
+                if (null != mOnItemLongClickListener) {
+                    holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+                            return mOnItemLongClickListener.onItemLongCLick(holder, position - getHeadersCount());
+                        }
+                    });
+                }
+            } else {
+                if (null != mLayoutManager && mLayoutManager instanceof StaggeredGridLayoutManager) {
+                    StaggeredGridLayoutManager.LayoutParams params = new StaggeredGridLayoutManager.LayoutParams(
+                            StaggeredGridLayoutManager.LayoutParams.MATCH_PARENT,
+                            StaggeredGridLayoutManager.LayoutParams.MATCH_PARENT);
+                    params.setFullSpan(true);
+                    holder.itemView.setLayoutParams(params);
+                }
+            }
+        }
     }
 
     @Override
@@ -26,4 +102,53 @@ public class PtrAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         return 0;
     }
 
+
+    private RecyclerView.AdapterDataObserver mDataObserver = new RecyclerView.AdapterDataObserver() {
+
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+            super.onItemRangeChanged(positionStart, itemCount);
+            notifyItemRangeChanged(positionStart + getHeadersCount(), itemCount);
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
+            notifyItemRangeInserted(positionStart + getHeadersCount(), itemCount);
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            super.onItemRangeRemoved(positionStart, itemCount);
+            notifyItemRangeRemoved(positionStart + getHeadersCount(), itemCount);
+        }
+
+        @Override
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+            super.onItemRangeMoved(fromPosition, toPosition, itemCount);
+            notifyItemRangeChanged(fromPosition + getHeadersCount(), toPosition + getHeadersCount() + itemCount);
+        }
+    };
+
+    public interface OnItemClickListener {
+        void onItemClick(RecyclerView.ViewHolder holder, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.mOnItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemLongClickListener {
+        boolean onItemLongCLick(RecyclerView.ViewHolder holder, int position);
+    }
+
+    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+        this.mOnItemLongClickListener = onItemLongClickListener;
+    }
 }
--- widgets/src/main/java/com/hezaijin/advance/widgets/view/ptr/load/PtrLoadMoreListener.java
+++ widgets/src/main/java/com/hezaijin/advance/widgets/view/ptr/load/PtrLoadMoreListener.java
@@ -3,7 +3,7 @@ package com.hezaijin.advance.widgets.view.ptr.load;
 import android.support.v7.widget.RecyclerView;
 
 /**
- *
+ * 可以判断滑动到最底部的时候，设置footview为加载中
  * @author HaoZhang
  * @date 2016/2/1.
  */
